2fc267a7cff1854aad8e982ddcd226a2ce7ea167
LiangDong <565498451@qq,com>
2020-02-05 15:48:09 +0800

    格式化

--- test1/venv/Mytest/function.py
+++ test1/venv/Mytest/function.py
@@ -1,12 +1,22 @@
 from functools import reduce
+
+
 def f(x, y):
     return x * y
+
+
 def g(x):
     return x ** 2
+
+
 def tran(name):
     return str.upper(name[0]) + str.lower(name[1:])
+
+
 def pord(lis):
-    return reduce(lambda x,y:x*y,lis)
+    return reduce(lambda x, y: x * y, lis)
+
+
 l1 = [1, 2, 3, 4, 5, 6, 7, 8]
 r = reduce(f, l1)
 print(r)
@@ -14,4 +24,4 @@ q = map(g, l1)
 print(list(q))
 a = ["asdfSSDa", "adsfSSS", "gSSasdfas"]
 print(list(map(tran, a)))
-print(pord([1,2,3,4]))
+print(pord([1, 2, 3, 4]))

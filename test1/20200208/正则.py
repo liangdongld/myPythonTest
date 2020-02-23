4f5244e8b433d6fea47b332801d91d5b44656602
LiangDong <565498451@qq,com>
2020-02-23 16:49:43 +0800

    正则表达式

--- test1/20200208/正则.py
+++ test1/20200208/正则.py
@@ -19,3 +19,46 @@
 # {n,m} 重复n到m次
 # 所有的量词只能跟在匹配规则的后边 先规则后量词
 # 量词后加上?表示非贪婪匹配
+import re
+
+# findall 返回所有满足匹配条件的结果，放在列表里
+# search 从前往后找到一个就返回，返回一个结果的对象，需要调用group方法才能获取
+# match 从最开始的位置匹配如果失败则返回none
+# sub 替换
+# subn 替换n个
+# compile 返回一个匹配对象
+# finditer 查找返回迭代器
+
+'''findall'''
+s = re.findall('([a-z]{3})+', 'defabcghijklmnopdqrstuvwxyz')
+for i in s:
+    print(i)
+
+'''search'''
+s = re.search('a[a-z]+z$', "ajlkjalkjlz")
+if s:
+    print(s.group())
+
+'''match'''
+s = re.match('\\d{1,3}', "123dfad123sadfa")
+if s:
+    print(s.group())
+
+'''sub'''
+s = re.sub('\\d{3}', 'hhh', '123dfasdf123')
+if s:
+    print(s)
+
+'''subn'''
+s = re.sub('\\d{3}', 'hhh', '123dfasdf123', 1)
+print(s)
+
+'''compile'''
+obj = re.compile('\\d{3}')
+ret = obj.search('123dfasdf123')
+print(ret.group())
+
+'''finditer'''
+s = re.finditer('\\d{3}', '123dfasdf456')
+for i in s:
+    print("哈哈哈：", i.group())  # 需要执行group方法
